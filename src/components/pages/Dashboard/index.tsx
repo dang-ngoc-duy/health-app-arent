a026fb62e35c41822de0aa4c174e2af6ef254988
Justin Dang <duydangit24@gmail.com>
2023-07-08 14:51:49 +0700

    Top page's layout overview

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,82 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledDashboard } from "./style";
 
 const Dashboard: React.FC = () => {
+  const [itemList, setItemList] = useState([
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+  ]);
+
+  const [filterList, setFilterList] = useState([
+    { name: "Filter 1", isActive: false },
+    { name: "Filter 2", isActive: false },
+    { name: "Filter 3", isActive: false },
+    { name: "Filter 4", isActive: false },
+  ]);
+
+  const addItemList = () => {
+    const listContainer = document.getElementById("item-list");
+
+    setItemList((prev) => [
+      ...prev,
+      { date: "05.21", mealsOfTheDay: "Morning", imageLink: "" },
+    ]);
+
+    listContainer && (listContainer.scrollTop = listContainer.scrollHeight);
+  };
+
   return (
     <StyledDashboard>
       <div className="pie-chart">Pie Chart</div>
       <div className="line-chart">Line Chart</div>
       <div className="container">
-        <div className="filters">Filters</div>
-        <div className="item-list">
-          Item List
-          <ul>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-            <li>Item 1</li>
-          </ul>
-          <button>Load More</button>
+        <div className="filters">
+          {filterList.map((filter, idx) => (
+            <div
+              key={idx}
+              style={{
+                width: "8.5rem",
+                height: "8.5rem",
+                background: "var(--primary-300-400)",
+              }}
+              className="filter"
+            >
+              {filter.name}
+            </div>
+          ))}
+        </div>
+        <div id="item-list" className="item-list">
+          {itemList.map((item, idx) => (
+            <div key={idx} style={{ background: "#FFCC21" }}>
+              {item.date}
+            </div>
+          ))}
+        </div>
+        <div className="load-more">
+          <button onClick={addItemList}>Load More</button>
         </div>
       </div>
     </StyledDashboard>
--- src/components/pages/Dashboard/style.tsx
+++ src/components/pages/Dashboard/style.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import PIE_CHART_BG from "src/assets/images/d01.jpg";
 
 export const StyledDashboard = styled.div`
   height: 100vh;
@@ -10,9 +11,11 @@ export const StyledDashboard = styled.div`
 
   .pie-chart {
     height: 100%;
+    background-image: url(${PIE_CHART_BG});
   }
   .line-chart {
     height: 100%;
+    background-color: var(--dark-600);
   }
   .container {
     margin: 0 10rem;
@@ -22,8 +25,30 @@ export const StyledDashboard = styled.div`
     grid-column-end: 3;
     overflow: hidden;
 
+    .filters {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 4rem;
+      margin-top: 1.5rem;
+      margin-bottom: 1.5rem;
+    }
+
     .item-list {
       overflow-y: auto;
+
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(14.625rem, 1fr));
+      grid-auto-rows: 14.625rem;
+      grid-gap: 0.5rem;
+    }
+
+    .load-more {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 1.75rem;
+      margin-bottom: 4rem;
     }
   }
 `;
